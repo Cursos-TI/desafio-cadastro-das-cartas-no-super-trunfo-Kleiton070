56ec62be1ce69cc4e74b367ad5bc07b63730c1cc
Kleiton070 <kleitonhenrique1984@gmail.com>
2025-05-31 23:21:09 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,56 @@
+#include <stdio.h>
 
+int main() {
+    // Variáveis da carta 1
+    int populacao1, pontos1;
+    float area1, pib1;
+
+    // Variáveis da carta 2
+    int populacao2, pontos2;
+    float area2, pib2;
+
+    // Cadastro da carta 1
+    printf("Cadastro da Carta 1:\n");
+    printf("Digite a populacao: ");
+    scanf("%d", &populacao1);
+
+    printf("Digite a area (em km²): ");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB (em bilhões): ");
+    scanf("%f", &pib1);
+
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &pontos1);
+
+    // Cadastro da carta 2
+    printf("\nCadastro da Carta 2:\n");
+    printf("Digite a populacao: ");
+    scanf("%d", &populacao2);
+
+    printf("Digite a area (em km²): ");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB (em bilhões): ");
+    scanf("%f", &pib2);
+
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &pontos2);
+
+    // Exibir os dados
+    printf("\n===== Dados das Cartas =====\n");
+
+    printf("\nCarta 1:\n");
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões\n", pib1);
+    printf("Pontos Turisticos: %d\n", pontos1);
+
+    printf("\nCarta 2:\n");
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões\n", pib2);
+    printf("Pontos Turisticos: %d\n", pontos2);
+
+    return 0;
+}
